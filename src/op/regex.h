3cec006d19ee81f85db22aaae3424929fe755ab1
devender singh <devender.singh@verizondigitalmedia.com>
2018-08-01 14:47:13 -0700

    if pcre_jit is not enabled, check the error codes instead of pcre_extra value

--- src/op/regex.h
+++ src/op/regex.h
@@ -95,7 +95,13 @@ public:
                 m_regex_study = pcre_study(m_regex,
                                            s_pcre_study_options,
                                            &m_err_ptr);
-                if(!m_regex_study)
+                // From man page:
+                // If the function returns NULL, 
+                // either it could not find any additional information, or there was an error.
+                // You can tell the difference by looking at the error value. It is NULL in first case.
+                //
+                // So we check whether it throws an error. Its null if its not compiled with JIT.
+                if(m_err_ptr)
                 {
                         return WAFLZ_STATUS_ERROR;
                 }

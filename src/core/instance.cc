53a9f5dfa3ff20eb849d7c00cd1a9684d67aed53
dev0z <devendersinghchoudhary@gmail.com>
2018-12-06 15:08:10 -0800

    adding a check

--- src/core/instance.cc
+++ src/core/instance.cc
@@ -404,7 +404,10 @@ int32_t instance::process(waflz_pb::event **ao_audit_event,
         }
         // reset phase 1
         // -------------------------------------------------
-        l_s = l_rqst_ctx->reset_phase_1();
+        if(l_rqst_ctx)
+        {
+                l_s = l_rqst_ctx->reset_phase_1();
+        }
         // -------------------------------------------------
         // *************************************************
         //                     P R O D

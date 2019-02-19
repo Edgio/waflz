e7bf741729667569d7d8c7bbf205c7bf61e13eba
dev0z <devendersinghchoudhary@gmail.com>
2019-02-18 16:35:46 -0800

    need to sync submodules first

--- build_simple.sh
+++ build_simple.sh
@@ -27,6 +27,15 @@ which cmake || {
     echo "Failed to find all required apps to build (cmake)."
     exit 1
 }
+# This is necessary in scenarios where the URL of the remote for a given submodule has changed.
+git submodule sync || {
+    echo "FAILED TO SYNC IS2 LIB"
+    exit 1
+}
+git submodule update -f --init || {
+    echo "FAILED TO UPDATE TO LATEST IS2 LIB"
+    exit 1
+}
 mkdir -p build
 pushd build && \
     cmake ../ \

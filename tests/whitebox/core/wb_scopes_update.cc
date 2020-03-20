a8909645989c6dd56906a1524800f47087c19cb8
RevathiSaba <revathi.sabanayagam@gmail.com>
2020-03-20 14:52:07 -0700

    adding placeholders for whitebox tests

---
+++ tests/whitebox/core/wb_scopes_update.cc
@@ -0,0 +1,35 @@
+//: ----------------------------------------------------------------------------
+//: Copyright (C) 2017 Verizon.  All Rights Reserved.
+//: All Rights Reserved
+//:
+//: \file:    wb_scopes_update.cc
+//: \details: To test all entity updates in scopes
+//: \author:  Revathi Sabanayagam
+//: \date:    12/30/2017
+//:
+//:   Licensed under the Apache License, Version 2.0 (the "License");
+//:   you may not use this file except in compliance with the License.
+//:   You may obtain a copy of the License at
+//:
+//:       http://www.apache.org/licenses/LICENSE-2.0
+//:
+//:   Unless required by applicable law or agreed to in writing, software
+//:   distributed under the License is distributed on an "AS IS" BASIS,
+//:   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//:   See the License for the specific language governing permissions and
+//:   limitations under the License.
+//:
+//: ----------------------------------------------------------------------------
+//: ----------------------------------------------------------------------------
+//: includes
+//: ----------------------------------------------------------------------------
+#include "catch/catch.hpp"
+#include "waflz/scopes_configs.h"
+#include "waflz/scopes.h"
+//: ----------------------------------------------------------------------------
+//: Test to update acl config
+//: ----------------------------------------------------------------------------
+TEST_CASE( "acl config update", "[acl config]" )
+{
+		
+}

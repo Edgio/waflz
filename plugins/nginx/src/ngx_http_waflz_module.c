adb0d57531ceffbc529e929e963a1022314b7f82
dsingh <devender.singh@verizondigitalmedia.com>
2020-06-18 17:02:18 -0700

    removing rqst_id from everywhere

--- plugins/nginx/src/ngx_http_waflz_module.c
+++ plugins/nginx/src/ngx_http_waflz_module.c
@@ -244,14 +244,13 @@ static rqst_ctx_callbacks s_callbacks = {
     get_rqst_header_size_cb,        /* get_rqst_header_size_cb */
     NULL,                           /* get_rqst_header_w_key_cb */
     get_rqst_header_w_idx_cb,       /* get_rqst_header_w_idx_cb */
-    NULL,                           /* get_rqst_id_cb */
     NULL,                           /* get_rqst_body_str_cb */
     NULL,                           /* get_rqst_local_addr_cb */
     NULL,                           /* get_rqst_canonical_port_cb */
     NULL,                           /* get_rqst_apparent_cache_status_cb */
     NULL,                           /* get_rqst_bytes_out_cb */
     NULL,                           /* get_rqst_bytes_in_cb */
-    NULL,                           /* get_rqst_req_id_cb */
+    NULL,                           /* get_rqst_uuid_cb */
     NULL                            /* get_cust_id_cb */
 };
 

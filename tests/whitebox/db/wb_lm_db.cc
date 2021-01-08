83fc25136636e86b10af1ba325f96007ead11c65
RevathiSaba <revathi.sabanayagam@gmail.com>
2021-01-07 22:20:04 -0800

    fixing_test_failure

--- tests/whitebox/db/wb_lm_db.cc
+++ tests/whitebox/db/wb_lm_db.cc
@@ -24,47 +24,17 @@
 #define MONKEY_KEY "TEST::KEY::MONKEY::BONGO"
 #define BANANA_KEY "TEST::KEY::BANANA::SMELLY"
 #define TEST_KEY "TEST::KEY::TEST::NONE"
+//!-----------------------------------------------------------------------------
+//! get db dir path
 //! ----------------------------------------------------------------------------
-//! remove dir
-//! ----------------------------------------------------------------------------
-static int remove_dir(const std::string& a_db_dir)
+static void get_lmdb_path(std::string& a_path)
 {
-        int32_t l_s;
-        struct stat l_stat;
-        l_s = stat(a_db_dir.c_str(), &l_stat);
-        if(l_s != 0)
-        {
-                return 0;
-        }
-        std::string l_file1(a_db_dir), l_file2(a_db_dir);
-        l_file1.append("/data.mdb");
-        l_file2.append("/lock.mdb");
-        unlink(l_file1.c_str());
-        unlink(l_file2.c_str());
-        l_s = rmdir(a_db_dir.c_str());
-        if(l_s != 0)
-        {
-                return -1;
-        }
-        return 0;
+        std::string l_file(__FILE__);
+        size_t l_pos = l_file.find("whitebox");
+        a_path.assign(l_file.substr(0, l_pos));
+        a_path.append("data/waf/db/test_lmdb");
 }
-//! ----------------------------------------------------------------------------
-//! mkdir
-//! ----------------------------------------------------------------------------
-static int create_dir(const std::string& a_db_dir)
-{
-        struct stat l_stat;
-        int32_t l_s;
-        l_s = remove_dir(a_db_dir);
-        if(l_s != 0)
-        {
-                return -1;
-        }
-        l_s = mkdir(a_db_dir.c_str(), 0700);
-        return l_s;
-}
-
-//!---------------------------------------------------------------------------
+//!-----------------------------------------------------------------------------
 //! lm_db
 //! ----------------------------------------------------------------------------
 TEST_CASE( "lmdb test", "[lmdb]" ) {
@@ -82,115 +52,115 @@ TEST_CASE( "lmdb test", "[lmdb]" ) {
                 int32_t l_s;
                 ns_waflz::lm_db l_db;
                 REQUIRE((l_db.get_init() == false));
-                std::string l_db_dir("/tmp/test_lmdb");
-                l_s = create_dir(l_db_dir);
-                REQUIRE((l_s == 0));
+                // get lmdb dir path
+                std::string l_db_dir;
+                get_lmdb_path(l_db_dir);
+                //init
+                l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_DIR_PATH, l_db_dir.c_str(), l_db_dir.length());
+                l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_READERS, NULL, 6);
+                l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_MMAP_SIZE, NULL, 10485760);
+                l_s = l_db.init();
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+        }
+        SECTION("validate increment key and expiration") {
+                int32_t l_s;
+                ns_waflz::lm_db l_db;
+                REQUIRE((l_db.get_init() == false));
+                // get lmdb dir path
+                std::string l_db_dir;
+                get_lmdb_path(l_db_dir);
+                //init
                 l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_DIR_PATH, l_db_dir.c_str(), l_db_dir.length());
                 l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_READERS, NULL, 6);
                 l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_MMAP_SIZE, NULL, 10485760);
                 l_s = l_db.init();
                 REQUIRE(l_s == WAFLZ_STATUS_OK);
                 REQUIRE(l_db.get_init() == true);
-                l_s = remove_dir(l_db_dir);
-                REQUIRE((l_s == 0));
-        }
-        SECTION("validate increment key and expiration") {
-            int32_t l_s;
-            ns_waflz::lm_db l_db;
-            REQUIRE((l_db.get_init() == false));
-            std::string l_db_dir("/tmp/test_lmdb");
-            l_s = create_dir(l_db_dir);
-            REQUIRE((l_s == 0));
-            l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_DIR_PATH, l_db_dir.c_str(), l_db_dir.length());
-            l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_READERS, NULL, 6);
-            l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_MMAP_SIZE, NULL, 10485760);
-            l_s = l_db.init();
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_db.get_init() == true);
-            int64_t l_result;
-            l_s = l_db.increment_key(l_result, MONKEY_KEY, 2000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 1);
-            l_s = l_db.increment_key(l_result, MONKEY_KEY, 2000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 2);
-            l_s = l_db.increment_key(l_result, BANANA_KEY, 4000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 1);
-            l_s = l_db.increment_key(l_result, BANANA_KEY, 4000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 2);
-            //sleep for 2 seconds, monkey key should have been expired
-            usleep(2000000);
-            //increment the test key, this should have cleared monkey key
-            l_s = l_db.increment_key(l_result, TEST_KEY, 1000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            l_s = l_db.get_key(l_result, MONKEY_KEY, strlen(MONKEY_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_ERROR);
-            l_s = l_db.get_key(l_result, BANANA_KEY, strlen(BANANA_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 2);
-            //sleep for 2 more seconds, banana key should have been expired
-            usleep(2000000);
-            //increment the test key, this should have cleared banana key
-            l_s = l_db.increment_key(l_result, TEST_KEY, 1000);
-            l_s = l_db.get_key(l_result, BANANA_KEY, strlen(BANANA_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_ERROR);
-            //increment the keys again to check value resets to 1.
-            l_s = l_db.increment_key(l_result, MONKEY_KEY, 1000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 1);
-            l_s = l_db.increment_key(l_result, BANANA_KEY, 1000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 1);
-            l_s = remove_dir(l_db_dir);
-            REQUIRE(l_s == 0);
+                int64_t l_result;
+                l_s = l_db.increment_key(l_result, MONKEY_KEY, 2000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 1);
+                l_s = l_db.increment_key(l_result, MONKEY_KEY, 2000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 2);
+                l_s = l_db.increment_key(l_result, BANANA_KEY, 4000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 1);
+                l_s = l_db.increment_key(l_result, BANANA_KEY, 4000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 2);
+                //sleep for 2 seconds, monkey key should have been expired
+                usleep(2000000);
+                //increment the test key, this should have cleared monkey key
+                l_s = l_db.increment_key(l_result, TEST_KEY, 1000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                l_s = l_db.get_key(l_result, MONKEY_KEY, strlen(MONKEY_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_ERROR);
+                l_s = l_db.get_key(l_result, BANANA_KEY, strlen(BANANA_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 2);
+                //sleep for 2 more seconds, banana key should have been expired
+                usleep(2000000);
+                //increment the test key, this should have cleared banana key
+                l_s = l_db.increment_key(l_result, TEST_KEY, 1000);
+                l_s = l_db.get_key(l_result, BANANA_KEY, strlen(BANANA_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_ERROR);
+                //increment the keys again to check value resets to 1.
+                l_s = l_db.increment_key(l_result, MONKEY_KEY, 1000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 1);
+                l_s = l_db.increment_key(l_result, BANANA_KEY, 1000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 1);
         }
         SECTION("validate sweep db - test if sweeping deletes expired keys from db") {
-            int32_t l_s;
-            ns_waflz::lm_db l_db;
-            REQUIRE((l_db.get_init() == false));
-            std::string l_db_dir("/tmp/test_lmdb");
-            l_s = create_dir(l_db_dir);
-            REQUIRE((l_s == 0));
-            l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_DIR_PATH, l_db_dir.c_str(), l_db_dir.length());
-            l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_READERS, NULL, 6);
-            l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_MMAP_SIZE, NULL, 10485760);
-            l_s = l_db.init();
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_db.get_init() == true);
-            int64_t l_result;
-            l_s = l_db.increment_key(l_result, MONKEY_KEY, 4000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 1);
-            l_s = l_db.increment_key(l_result, BANANA_KEY, 5000);
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_result == 1);
-            // sweep db 
-            l_s = l_db.sweep_db();
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            int64_t l_out_val = -1;
-            //verify sweep db didn't delete keys before expiry
-            l_s = l_db.get_key(l_out_val, MONKEY_KEY, strlen(MONKEY_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_out_val == 1);
-            l_s = l_db.get_key(l_out_val, BANANA_KEY, strlen(BANANA_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            REQUIRE(l_out_val == 1);
-            // sleep for 4 seconds and sweep db.Monkey key should have been
-            //deleted
-            sleep(4);
-            l_s = l_db.sweep_db();
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            l_s = l_db.get_key(l_out_val, MONKEY_KEY, strlen(MONKEY_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_ERROR);
-            l_s = l_db.get_key(l_out_val, BANANA_KEY, strlen(BANANA_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            sleep(1);
-            l_s = l_db.sweep_db();
-            REQUIRE(l_s == WAFLZ_STATUS_OK);
-            l_s = l_db.get_key(l_out_val, BANANA_KEY, strlen(BANANA_KEY));
-            REQUIRE(l_s == WAFLZ_STATUS_ERROR);
+                int32_t l_s;
+                ns_waflz::lm_db l_db;
+                REQUIRE((l_db.get_init() == false));
+                // get lmdb dir path
+                std::string l_db_dir;
+                get_lmdb_path(l_db_dir);
+                //init
+                l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_DIR_PATH, l_db_dir.c_str(), l_db_dir.length());
+                l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_READERS, NULL, 6);
+                l_db.set_opt(ns_waflz::lm_db::OPT_LMDB_MMAP_SIZE, NULL, 10485760);
+                l_s = l_db.init();
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_db.get_init() == true);
+                int64_t l_result;
+                l_s = l_db.increment_key(l_result, MONKEY_KEY, 2000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 1);
+                l_s = l_db.increment_key(l_result, BANANA_KEY, 3000);
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_result == 1);
+                // sweep db
+                l_s = l_db.sweep_db();
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                int64_t l_out_val = -1;
+                //verify sweep db didn't delete keys before expiry
+                l_s = l_db.get_key(l_out_val, MONKEY_KEY, strlen(MONKEY_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_out_val == 1);
+                l_s = l_db.get_key(l_out_val, BANANA_KEY, strlen(BANANA_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                REQUIRE(l_out_val == 1);
+                // sleep for 2 seconds and sweep db. Monkey key should have been
+                //deleted
+                usleep(2000000);
+                l_s = l_db.sweep_db();
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                l_s = l_db.get_key(l_out_val, MONKEY_KEY, strlen(MONKEY_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_ERROR);
+                l_s = l_db.get_key(l_out_val, BANANA_KEY, strlen(BANANA_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                // sleep for 1 second and sweep db. Banana key should have been
+                // deleted
+                usleep(1000000);
+                l_s = l_db.sweep_db();
+                REQUIRE(l_s == WAFLZ_STATUS_OK);
+                l_s = l_db.get_key(l_out_val, BANANA_KEY, strlen(BANANA_KEY));
+                REQUIRE(l_s == WAFLZ_STATUS_ERROR);
         }
 }
 

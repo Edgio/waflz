1a9d8ae7d9dc36d5b26ea2b92fab73ebe7cc04e9
Reed Morrison <reed.morrison@gmail.com>
2021-11-04 20:38:29 -0700

    Org update pt2 (#258)
    * org update.
    
    * org update.

--- include/waflz/render.h
+++ include/waflz/render.h
@@ -1,5 +1,5 @@
 //! ----------------------------------------------------------------------------
-//! Copyright (C) 2018 Verizon.  All Rights Reserved.
+//! Copyright (C) 2018 Edgecast Inc.  All Rights Reserved.
 //! All Rights Reserved
 //! \file:    render_resp.h
 //! \details: TODO

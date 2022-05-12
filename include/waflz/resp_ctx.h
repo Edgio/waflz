1d3982c6731225888714a3ad0d526c8c8aaea6b2
kmodi <kanishk.modi@verizondigitalmedia.com>
2022-05-11 18:47:36 -0700

    create resp_ctx header/cc stubs

---
+++ include/waflz/resp_ctx.h
@@ -0,0 +1,145 @@
+//! ----------------------------------------------------------------------------
+//! Copyright Edgecast Inc.
+//!
+//! \file:    TODO
+//! \details: TODO
+//!
+//! Licensed under the terms of the Apache 2.0 open source license.
+//! Please refer to the LICENSE file in the project root for the terms.
+//! ----------------------------------------------------------------------------
+#ifndef _RESP_CTX_H
+#define _RESP_CTX_H
+//! ----------------------------------------------------------------------------
+//! includes
+//! ----------------------------------------------------------------------------
+#include <waflz/def.h>
+#ifdef __cplusplus
+#include <waflz/arg.h>
+#include <waflz/parser.h>
+#include <waflz/profile.h>
+#include <list>
+#include <map>
+#include <strings.h>
+#endif
+
+//! ----------------------------------------------------------------------------
+//! fwd decl's
+//! ----------------------------------------------------------------------------
+#ifndef __cplusplus
+typedef struct resp_ctx_t resp_ctx;
+#endif
+#ifdef __cplusplus
+namespace waflz_pb {
+class event;
+}
+namespace waflz_pb {
+class limit;
+class condition_group;
+}
+namespace ns_waflz {
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef struct {
+        get_resp_data_cb_t m_get_resp_content_type_list_cb;
+        get_resp_data_cb_t m_get_resp_content_length_cb;
+}resp_ctx_callbacks;
+#ifdef __cplusplus
+}
+#endif
+//! ----------------------------------------------------------------------------
+//! fwd decl's
+//! ----------------------------------------------------------------------------
+#ifdef __cplusplus
+class waf;
+//! ----------------------------------------------------------------------------
+//! types
+//! ----------------------------------------------------------------------------
+struct cx_case_i_comp
+{
+        bool operator() (const std::string& lhs, const std::string& rhs) const
+        {
+                return strcasecmp(lhs.c_str(), rhs.c_str()) < 0;
+        }
+};
+typedef std::map<std::string, std::string, cx_case_i_comp> cx_map_t;
+typedef std::map <data_t, data_t, data_case_i_comp> data_map_t;
+typedef std::list<data_t> data_list_t;
+//! ----------------------------------------------------------------------------
+//! xpath optimization
+//! ----------------------------------------------------------------------------
+typedef std::list <const_arg_t> xpath_arg_list_t;
+typedef std::map <std::string, xpath_arg_list_t> xpath_cache_map_t;
+//! ----------------------------------------------------------------------------
+//! resp_ctx
+//! ----------------------------------------------------------------------------
+class resp_ctx
+{
+public:
+        // -------------------------------------------------
+        // callbacks
+        // -------------------------------------------------
+        static get_data_cb_t s_get_bot_ch_prob;
+        // -------------------------------------------------
+        // static members
+        // -------------------------------------------------
+        static uint32_t s_body_arg_len_cap;
+        // -------------------------------------------------
+        // public methods
+        // -------------------------------------------------
+        resp_ctx(void *a_ctx,
+                 uint32_t a_body_len_max,
+                 const resp_ctx_callbacks *a_callbacks,
+                 bool a_parse_xml = false,
+                 bool a_parse_json = false);
+        ~resp_ctx();
+
+        int32_t reset_phase_1();
+        void show(void);
+        // -------------------------------------------------
+        // public members
+        // -------------------------------------------------
+        uint32_t m_an;
+        uint64_t m_content_length;
+        data_list_t m_content_type_list;
+        uint32_t m_body_len_max;
+        char *m_body_data;
+        uint32_t m_body_len;
+        uint32_t m_resp_status;
+        // -------------------------------------------------
+        // TODO FIX!!! -not thread safe...
+        // -------------------------------------------------
+        const waflz_pb::limit* m_limit;
+        // -------------------------------------------------
+        // body parser
+        // -------------------------------------------------
+        parser *m_body_parser;
+        // -------------------------------------------------
+        // response ctx callbacks struct
+        // -------------------------------------------------
+        const resp_ctx_callbacks *m_callbacks;
+
+private:
+        // -------------------------------------------------
+        // private methods
+        // -------------------------------------------------
+        // disallow copy/assign
+        resp_ctx(const resp_ctx &);
+        resp_ctx& operator=(const resp_ctx &);
+        // -------------------------------------------------
+        // private members
+        // -------------------------------------------------
+        void *m_ctx;
+};
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+resp_ctx *init_resp_ctx(void *a_ctx, const uint32_t a_max_body_len, const resp_ctx_callbacks *a_callbacks, bool a_parse_json);
+int32_t resp_ctx_cleanup(resp_ctx *a_resp_ctx);
+#ifdef __cplusplus
+}
+}
+#endif
+#endif
---
+++ src/core/resp_ctx.cc
@@ -0,0 +1,78 @@
+//! ----------------------------------------------------------------------------
+//! Copyright Edgecast Inc.
+//!
+//! \file:    TODO
+//! \details: TODO
+//!
+//! Licensed under the terms of the Apache 2.0 open source license.
+//! Please refer to the LICENSE file in the project root for the terms.
+//! ----------------------------------------------------------------------------
+//! ----------------------------------------------------------------------------
+//! includes
+//! ----------------------------------------------------------------------------
+#include "event.pb.h"
+#include "waflz/def.h"
+#include "waflz/resp_ctx.h"
+#include "waflz/geoip2_mmdb.h"
+#include "waflz/string_util.h"
+#include "core/decode.h"
+#include "op/regex.h"
+#include "support/ndebug.h"
+#include "support/time_util.h"
+#include "parser/parser_url_encoded.h"
+#include "parser/parser_xml.h"
+#include "parser/parser_json.h"
+#include <stdlib.h>
+#include <string.h>
+//! ----------------------------------------------------------------------------
+//! constants
+//! ----------------------------------------------------------------------------
+#define _DEFAULT_BODY_ARG_LEN_CAP 4096
+//! ----------------------------------------------------------------------------
+//! macros
+//! ----------------------------------------------------------------------------
+
+
+//! ----------------------------------------------------------------------------
+//! static
+//! ----------------------------------------------------------------------------
+uint32_t resp_ctx::s_body_arg_len_cap = _DEFAULT_BODY_ARG_LEN_CAP;
+get_data_cb_t resp_ctx::s_get_bot_ch_prob = NULL;
+
+//! ----------------------------------------------------------------------------
+//! \details: TODO
+//! \return:  TODO
+//! \param:   TODO
+//! ----------------------------------------------------------------------------
+resp_ctx::resp_ctx(void *a_ctx,
+                   uint32_t a_body_len_max,
+                   const resp_ctx_callbacks *a_callbacks,
+                   bool a_parse_xml,
+                   bool a_parse_json):
+        m_an(),
+        m_content_length(0),
+        m_content_type_list(),
+        m_body_len_max(a_body_len_max),
+        m_body_data(NULL),
+        m_body_len(0),
+        m_body_parser(),
+
+        // -------------------------------------------------
+        // *************************************************
+        // xml optimization
+        // *************************************************
+        // -------------------------------------------------
+        m_callbacks(a_callbacks),
+        
+        m_ctx(a_ctx)
+{
+}
+//! ----------------------------------------------------------------------------
+//! \details: TODO
+//! \return:  TODO
+//! \param:   TODO
+//! ----------------------------------------------------------------------------
+resp_ctx::~resp_ctx()
+{
+
+}
